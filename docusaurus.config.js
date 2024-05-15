abd4b19a430864d4da004c931e26db2d2860e082
chipqp <gromayaroslav@gmail.com>
2024-05-15 11:26:48 +0300

    changed slogan

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Unique Valley',
-  tagline: 'UNIQUE VINTAGE ITEMS',
+  tagline: 'MILLION OF UNIQUE VINTAGE ITEMS',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
